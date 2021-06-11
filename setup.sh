4b50e94ebe268a606a308a2cbb45596da704f202
TUNAPRO1234 <tunagul54@gmail.com>
2021-06-11 06:49:57 +0000

    badababada

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ cp rc.local /etc/rc.local
 
 # vim setup ehehehe
 apt install vim -y
-cp -r .vim ~/.vim
-cp .vimrc ~/.vimrc
+
+cp -r .vim /home/pi/.vim
+cp .vimrc /home/pi/.vimrc
+
+cp -r .vim root/.vim
+cp .vimrc root/.vimrc
+
 echo ok
